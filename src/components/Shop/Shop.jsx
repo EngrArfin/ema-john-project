4daebfd7393ca7ac86749fcdfd84072905e61bf7
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2023-05-17 18:41:43 +0600

    Project Update

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h2>Product Come Here</h2>
+            </div>
+            <div className="cart-container">
+                <h3>Product Summary</h3>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
