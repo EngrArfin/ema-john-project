c14fc9ea0936554d5d583100d622c1aab9ed426f
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2023-05-18 10:52:38 +0600

    complete Shop Product Cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css'
+import Product from '../Product/Product';
 
 const Shop = () => {
 
@@ -14,7 +15,12 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h2>Product Come Here:{products.length}</h2>
+                {
+                    products.map(product => <Product
+                    key={product.id}
+                    product={product}
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h3>Product Summary</h3>
